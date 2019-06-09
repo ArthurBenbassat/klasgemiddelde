a3c2bd0a046cea2ecd64cd193c797ce8dc01e30c
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-06-09 16:22:29 +0200

    Done!

--- klasgemiddelde/Module1.vb
+++ klasgemiddelde/Module1.vb
@@ -5,25 +5,45 @@
         Dim punten(2, 3) As Integer
         Dim gemiddelde As Integer
         Dim totaalStudent(3) As Integer
-        For index As Integer = 0 To 3
-            Console.Write("Wat is de naam? ")
-            namen(index) = Console.ReadLine()
-            Console.Write("Wat is het eerste punt?")
-            punten(0, index) = Console.ReadLine
-            Console.Write("Wat is het tweede punt?")
-            punten(1, index) = Console.ReadLine
-            Console.Write("Wat is het derde punt?")
-            punten(2, index) = Console.ReadLine
+        Dim aantalln, aantaltesten As Integer
 
-            For teller As Integer = 0 To 2
+
+        Console.Write("Hoeveel leerlingen zitten er in de klas? ")
+        aantalln = Console.ReadLine()
+        Console.Write("Hoeveel testen zijn er? ")
+        aantaltesten = Console.ReadLine()
+
+        ReDim namen(aantalln)
+        ReDim punten(aantaltesten, aantalln)
+        ReDim totaalStudent(aantalln)
+
+        For index As Integer = 0 To aantalln - 1
+            Console.Write(vbCrLf & "Wat is de naam? ")
+            namen(index) = Console.ReadLine()
+            For teller As Integer = 0 To aantaltesten - 1
+                Console.Write("Hoeveel had " & namen(index) & " op test " & teller + 1 & "? ")
+                punten(teller, index) = Console.ReadLine
+            Next
+            For teller As Integer = 0 To aantaltesten - 1
                 totaalStudent(index) += punten(teller, index)
             Next
             gemiddelde += totaalStudent(index)
         Next
-        For teller As Integer = 0 To 3
-            Console.WriteLine("Het gemiddelde van de leerling: " & namen(teller) & " is " & totaalStudent(teller) / 3)
+        Console.WriteLine()
+        For teller As Integer = 0 To aantalln - 1
+            Console.WriteLine("Het gemiddelde van de leerling: " & namen(teller) & " is " & (totaalStudent(teller) / aantaltesten - 1) * 5 & "%")
         Next
-        Console.Write(vbCrLf & "Het totaal gemiddelde is " & (gemiddelde / 3) / 4)
+        Console.Write(vbCrLf & "Het totaal gemiddelde is " & ((gemiddelde / aantaltesten - 1) / aantalln - 1) * 5 & "%")
+
+        FileOpen(1, "C:\Users\Arthur\Documents\School\informatica\klasResulaten.txt", OpenMode.Append)
+        For index As Integer = 0 To aantalln - 1
+            PrintLine(1, namen(index))
+            For teller As Integer = 0 To aantaltesten - 1
+                PrintLine(1, "Test " & teller + 1 & ": " & punten(teller, index))
+            Next
+            PrintLine(1, vbCrLf & "Het gemiddelde: " & namen(index) & " is " & (totaalStudent(index) / aantaltesten - 1) * 5 & "%" & vbCrLf)
+        Next
+        PrintLine(1, vbCrLf & "Het totaal gemiddelde is " & ((gemiddelde / aantaltesten - 1) / aantalln - 1) * 5 & "%")
         Console.ReadLine()
     End Sub
 End Module
