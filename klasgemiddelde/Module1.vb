9661450adef7c61f6c59d4509d8723349c416287
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-06-09 12:43:12 +0200

    afgewerkt

--- klasgemiddelde/Module1.vb
+++ klasgemiddelde/Module1.vb
@@ -1,11 +1,11 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim namen() As String
-        Dim punten(,) As Integer
-        Dim gemiddelde() As Integer
-        Dim totaalStudent() As Integer
-        For index As Integer = 0 To 10
+        Dim namen(3) As String
+        Dim punten(2, 3) As Integer
+        Dim gemiddelde As Integer
+        Dim totaalStudent(3) As Integer
+        For index As Integer = 0 To 3
             Console.Write("Wat is de naam? ")
             namen(index) = Console.ReadLine()
             Console.Write("Wat is het eerste punt?")
@@ -16,12 +16,14 @@
             punten(2, index) = Console.ReadLine
 
             For teller As Integer = 0 To 2
-                totaalStudent(index) = punten(1, teller)
+                totaalStudent(index) += punten(teller, index)
             Next
+            gemiddelde += totaalStudent(index)
         Next
-        For teller As Integer = 0 To 10
-            Console.Write("Het totaal van de leerling: " & namen(teller) & " is " & totaalStudent(teller))
+        For teller As Integer = 0 To 3
+            Console.WriteLine("Het gemiddelde van de leerling: " & namen(teller) & " is " & totaalStudent(teller) / 3)
         Next
+        Console.Write(vbCrLf & "Het totaal gemiddelde is " & (gemiddelde / 3) / 4)
+        Console.ReadLine()
     End Sub
-'denis stinkt
 End Module
